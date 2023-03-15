27454059a84f59ad0ecf3fdbf01b77fd3bcba101
Ricardo Soriano <ricardo.roel110@gmail.com>
2023-03-14 20:12:21 -0600

    el cambio de labels funciona

--- Main.js
+++ Main.js
@@ -7,26 +7,31 @@ const Option = document.getElementById("selection")
 
 //? Intercambio de valores en inputs
 Option.addEventListener("change", () => {
+    let selectedIndex = Option.selectedIndex
+    
     //Etiquetas de campo
     const lblOne = document.getElementById("lbl1")
     const lblTwo = document.getElementById("lbl2")
+
     //Campos
     const Mass = document.getElementById("Mass-imp")
     const Accel = document.getElementById("Accel-imp")
 
-    if(Option == 0){
+    if(selectedIndex == 0){
         lblOne.innerHTML = "Masa:"
         lblTwo.innerHTML = "Aceleración:"
         Mass.setAttribute("placeholder", "Kg")
         Accel.setAttribute("placeholder", "m/s²")
     }
-    if(Option == 1){
+    
+    if(selectedIndex == 1){
         lblOne.innerHTML = "Peso:"
         lblTwo.innerHTML = "Aceleración:"
         Mass.setAttribute("placeholder", "N")
         Accel.setAttribute("placeholder", "m/s²")
     }
-    if(Option == 2){
+    
+    if(selectedIndex == 2){
         lblOne.innerHTML = "Peso:"
         lblTwo.innerHTML = "Masa:"
         Mass.setAttribute("placeholder", "N")
