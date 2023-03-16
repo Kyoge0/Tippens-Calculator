851d242cec8139fc0d37bac741a19a4b6d32c30d
Kyoge0 <kyogesanada@gmail.com>
2023-03-15 21:22:12 -0600

    add existing file

--- Tippens.php
+++ Tippens.php
@@ -2,33 +2,34 @@
 <html>
     <head>
         <title>Calculadora de Tippens</title>
+        <link rel="stylesheet "href="styles.css">
         <meta charset="UTF-8">
     </head>
 
     <body>
         <div id="form-cont" name="form-cont">
             <form action="Results.php" method="get" id="form" class="form">
-                <div class="SystemOU">
+                <div class="container-flex">
                     <input type="radio" class="radio" value="IS" name="Units" id="IS" checked>
-                    <label for="IS">SI</label>
+                    <label for="IS" class="form-label">SI</label>
                     <input type="radio" class="radio" value="US" name="Units" id="US"></input>
-                    <label for="US">US</label>
+                    <label for="US" class="form-label">US</label>
                 </div>
-                <div class="selection-cont">
-                    <select name="selection" id="selection">
+                <div class="container-flex">
+                    <select name="selection" id="selection" class="form-select">
                         <option value="0" selected>Calculo de peso</option>
                         <option value="1">Calculo de masa</option>
                         <option value="2">Calculo de Aceleración</option>
                     </select>
                 </div>
-                <div class="Input-cont">
-                    <label for="Mass" id="lbl1">Masa:</label>
-                    <input type="number" step="any" placeholder="Kg" name="Mass" id="Mass-imp">
-                    <label for="Accel" id="lbl2">Aceleración:</label>
-                    <input type="number" step="any" placeholder="m/s²" name="Accel" id="Accel-imp">
-                    <input type="submit" value="Calcular">
-                    <label for="res">Resultado:</label>
-                    <input type="text" placeholder="resultado" name="result" id="result-imp" value ="<?php echo $Result ?>">
+                <div class="Icontainer-flex">
+                    <label for="Mass" id="lbl1" class="form-label">Masa:</label>
+                    <input type="number" step="any" placeholder="Kg" name="Mass" id="Mass-imp" class="form-imput">
+                    <label for="Accel" id="lbl2" class="form-label">Aceleración:</label>
+                    <input type="number" step="any" placeholder="m/s²" name="Accel" id="Accel-imp" class="form-imput">
+                    <input type="submit" value="Calcular" class="button">
+                    <label for="res" class="form-label" class="form-button">Resultado:</label>
+                    <input type="text" placeholder="resultado" name="result" id="result-imp" value ="<?php echo $Result ?>" class="form-imput">
                 </div>
             </form>
         </div>
