6071eaab5ec185e18d16a43e6960139f5da4a90f
Kyoge0 <kyogesanada@gmail.com>
2023-03-19 18:38:26 -0600

    add existing files

--- Results.php
+++ Results.php
@@ -22,9 +22,92 @@ if($_SERVER['REQUEST_METHOD']=='GET'){
     }
 
     if($Index == 2){
+        $Timp = $_GET['T-imp'];
+        $Vo = $_GET['Vel-o'];
+        $Vf = $_GET['Vel-f'];
+
+        if($selection == 0){
+            $Result = (($Vo + $Vf) / 2) * $Timp;
+        }
+        if($selection == 1){
+            
+        }
+        if($selection == 2){
+            
+        }
+        if($selection == 3){
+            
+        }
+
         include "vista/Posición-X-var1.php";
     }
+
+    if($Index == 3){
+        $Vo = $_GET['Vel-o'];
+        $Accel = $_GET['Accel'];
+        $Timp = $_GET['T-imp'];
+        
+        if($selection == 0){
+            $Result = ($Vo + ($Accel * $Timp));
+        }
+        if($selection == 1){
+            
+        }
+        if($selection == 2){
+            
+        }
+        if($selection == 3){
+            
+        }
+
+        include "vista/Velocidad-f.php";
+    }
     
+    if($Index == 4){
+        $Vo = $_GET['Vel-o'];
+        $Timp = $_GET['T-imp'];
+        $Accel = $_GET['Accel'];
+
+        if($selection == 0){
+            $Result = (($Vo * $Timp) + (0.5 * $Accel * ($Timp * $Timp)));
+        }
+        if($selection == 1){
+            
+        }
+        if($selection == 2){
+            
+        }
+        if($selection == 3){
+            
+        }
+
+        include "vista/Posición-X-var2.php";
+    }
+
+    if($Index == 5){
+        $Vo = $_GET['Vel-o'];
+        $Timp = $_GET['T-imp'];
+        $Accel = $_GET['Accel'];
+
+        if($selection == 0){
+            $Result = (($Vo * $Timp) - (0.5 * $Accel * ($Timp * $Timp)));
+        }
+        if($selection == 1){
+            
+        }
+        if($selection == 2){
+            
+        }
+        if($selection == 3){
+            
+        }
+
+        include "vista/Posición-X-var3.php";
+    }
+
+    if($Index == 6){
+
+    }
 }
 
 ?>
--- vista/Posición-X-var3.php
+++ vista/Posición-X-var3.php
@@ -19,21 +19,21 @@
                     <select name="selection" id="selection" class="form-select">
                         <option value="0" selected>Resolver para X</option>
                         <option value="1">Resolver para V₀</option>
-                        <option value="2">Resolver para Vf</option>
+                        <option value="2">Resolver para aceleración</option>
                         <option value="3">Resolver para t</option>
                     </select>
                 </div>
                 <div class="Icontainer-flex">
                     <label for="V₀" id="lbl1" class="form-label">Velocidad inicial:</label>
                     <input type="number" step="any" placeholder="m/s²" name="Vel-o" id="o-imp" class="form-imput">
-                    <label for="Vf" id="lbl2" class="form-label">Velocidad final:</label>
-                    <input type="number" step="any" placeholder="m/s²" name="Vel-f" id="f-imp" class="form-imput">
-                    <label for="T" id="lbl3" class="form-label">Tiempo:</label>
+                    <label for="T" id="lbl2" class="form-label">Tiempo:</label>
                     <input type="number" step="any" placeholder="S" name="T-imp" id="T-imp" class="form-imput">
+                    <label for="Accel" id="lbl3" class="form-label">Aceleración:</label>
+                    <input type="number" step="any" placeholder="m/s²" name="Accel" id="Accel" class="form-imput">
                     <input type="submit" value="Calcular" class="button">
                     <label for="res" class="form-label" class="form-button">Resultado:</label>
                     <input type="text" placeholder="resultado" name="result" id="result-imp" value="<?php echo $Result; ?>" class="form-imput">
-                    <input type="number" value="2" name="temp" hidden>
+                    <input type="number" value="5" name="temp" hidden>
                 </div>
             </form>
         </div>
